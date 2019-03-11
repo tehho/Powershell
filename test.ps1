448945c65599ff3711473eb62975f604075c0c42
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2019-03-11 16:26:28 +0100

    Testing

--- test.ps1
+++ test.ps1
@@ -1,22 +1,3 @@
 param()
 
-$json = Get-Content -Path "./test.json" | ConvertFrom-Json
-
-$ret = @{}
-
-foreach ($field in $json.fields)
-{
-  $value = $field.name.Split(".")[0]
-
-  if ($ret.ContainsKey($value))
-  {
-    $ret[$value] += 1
-  }
-  else {
-    $ret.Add($value, 1)
-  }
-}
-
-$ret | Sort-Object -Property Value
-
 exit 0
