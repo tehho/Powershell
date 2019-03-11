209ed755ba34b77a553c517488e4e9a41ef496a7
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2019-03-11 11:32:01 +0100

    refactoring

--- ssh-sudo-rm-filebeat-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for arg in "$@"; do
-  echo "SSH to remove /var/lib/filebeat-data/regbistry in node $arg"
-  ssh -oStrictHostKeyChecking=no azureuser@$arg sudo rm /var/lib/filebeat-data/registry
-done
