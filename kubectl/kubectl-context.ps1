56c5983eee5aadbfea06d2bb328c4bc720983c6a
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2019-04-10 13:17:13 +0200

    Fixed jobs for kubectl-context

--- kubectl/kubectl-context.ps1
+++ kubectl/kubectl-context.ps1
@@ -15,7 +15,11 @@ if ($args[0] -eq "--list")
 
 kubectl config use-context $args[0]
 
-$jobs = Get-Job | Where-Object {$_.Name.StartsWith("kubectl-proxy")} | Select-Object -ExpandProperty Id
-$jobs | ForEach-Object { remove-job -InstanceId $_ -Force }
-#kubectl proxy
-$job = Start-Job -Name "kubectl-proxy" -ScriptBlock {kubectl proxy}
+$jobs = Get-Job | Where-Object {$_.Name.StartsWith("kubectl-proxy")} 
+if ($jobs.count -ne 0)
+{
+    $jobs = $jobs | Select-Object -ExpandProperty Id
+    $jobs | ForEach-Object { remove-job -Id "$_" -Force }
+    #kubectl proxy
+    $job = Start-Job -Name "kubectl-proxy" -ScriptBlock {kubectl proxy}
+}
