444a44bb7580f26632c911909e0a79d98bdc2341
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2019-04-24 08:34:47 +0200

    Added choco update

---
+++ choco-update.ps1
@@ -0,0 +1,15 @@
+param()
+  choco.exe upgrade -y kubernetes-cli
+  choco.exe upgrade -y kubernetes-helm
+  choco.exe upgrade -y 7zip
+  choco.exe upgrade -y azure-cli
+  choco.exe upgrade -y choco.exelatey
+  choco.exe upgrade -y docker
+  choco.exe upgrade -y docker-cli
+  choco.exe upgrade -y docker-desktop
+  choco.exe upgrade -y docker-for-windows
+  choco.exe upgrade -y dotnetcore-sdk
+  choco.exe upgrade -y nodejs
+  choco.exe upgrade -y packer
+  choco.exe upgrade -y postman
+  choco.exe upgrade -y terraform 
