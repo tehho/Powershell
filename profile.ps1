99f1b9dea68c23c2a11fe5cee3e583bfafc3cabd
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2019-07-24 15:26:20 +0200

    added custom profile

---
+++ profile.ps1
@@ -0,0 +1,52 @@
+Set-Location -Path C:/Project
+
+function kubectl-context {
+  param (
+  )
+
+  if (($args.Count -eq 0) -or ($args[0] -eq "--list") -or ($args[0] -eq "-l"))
+  {
+    kubectl config get-contexts
+  }
+  else {
+    kubectl config use-context $args[0]
+    kubectl config set-context --current --namespace=kube-system
+    kubectl get pods
+  }
+}
+
+function kubectl-namespace {
+  param (
+  )
+
+  if (($args.Count -eq 0) -or ($args[0] -eq "--list") -or ($args[0] -eq "-l"))
+  {
+    kubectl get namespaces
+  }
+  else {
+    $namespace = $args[0]
+    kubectl config set-context --current --namespace="$namespace"
+    kubectl get pods
+  }
+}
+
+function kubectl-namespace-kube-system {
+  param (
+  )
+
+  kubectl-namespace "kube-system"
+}
+
+function kubectl-get-pods {
+  param (
+  )
+  
+  kubectl get pods
+}
+
+Set-Alias kx kubectl-context
+Set-Alias kn kubectl-namespace
+
+Set-Alias kgpo kubectl-get-pods
+
+Set-Alias k kubectl
